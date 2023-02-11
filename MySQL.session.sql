5c4f5223df9047911b29782d5ac8112b364c1d38
Ani180685 <ark180685@gmail.com>
2023-02-10 22:45:51 -0500

    Updated sql table

---
+++ MySQL.session.sql
@@ -0,0 +1,8 @@
+CREATE TABLE SoulMatch_Data.accounts(
+    Number_Of_Accounts INT Auto_Increment,
+    Email_ID VARCHAR(30) NOT NULL,
+    Password VARCHAR(10) NOT NULL, 
+    FirstName VARCHAR(10) NOT NULL,
+    LastName VARCHAR(10) NOT NULL,
+    PRIMARY KEY(Number_Of_Accounts)
+    );
