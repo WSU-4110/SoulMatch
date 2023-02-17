1b860fa44d69b19d2e6cbab67c6e4f27291fc6f4
Ani180685 <ark180685@gmail.com>
2023-02-17 15:08:21 -0500

    resolving error in starting frontend

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBugCNSBZ0FdxGUNG2LsRGYqAgCdPdCdyg",
+  authDomain: "soulmatch-d1c16.firebaseapp.com",
+  databaseURL: "https://soulmatch-d1c16-default-rtdb.firebaseio.com",
+  projectId: "soulmatch-d1c16",
+  storageBucket: "soulmatch-d1c16.appspot.com",
+  messagingSenderId: "508128078962",
+  appId: "1:508128078962:web:3e55a83c6fbbeab28766eb"
+};
+
+export default firebaseConfig
