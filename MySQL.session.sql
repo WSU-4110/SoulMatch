08b2e6d0faaff3f3d07e6c63a76f1b8b7cde7903
Anirudh <122410591+Ani180685@users.noreply.github.com>
2023-02-17 17:52:26 -0500

    Delete MySQL.session.sql
    deleting sql database

--- MySQL.session.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE SoulMatch_Data.accounts(
-    Number_Of_Accounts INT Auto_Increment,
-    Email_ID VARCHAR(30) NOT NULL,
-    Password_Key VARCHAR(10) NOT NULL, 
-    FirstName VARCHAR(10) NOT NULL,
-    LastName VARCHAR(10) NOT NULL,
-    PRIMARY KEY(Number_Of_Accounts)
-    );
