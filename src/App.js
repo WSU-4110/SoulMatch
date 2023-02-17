81de2732572ec79bdbd38c7d6e589ff212d0aed8
Mahin Haque <haqumahi27@gmail.com>
2023-02-17 18:11:27 -0500

    Added logo and Website title to top left of trait selection page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import AuthenticationPage from "./pages/AuthenticationPage";
+import PersonalizationPage from "./pages/PersonalizationPage";
 
 //TODO: Replace react logos with SoulMatch logos
 class App extends React.Component {
 
   render() {
     //TODO: Implement react router and routing to pages instead of only the login/register page
-    return (<AuthenticationPage />)
+    return (<PersonalizationPage />)
   }
 }
 
--- src/pages/PersonalizationPage.js
+++ src/pages/PersonalizationPage.js
@@ -2,3 +2,26 @@ import React, {useState} from "react";
 import '../styles/PersonalizationPage.css';
 
 
+class PersonalizationPage extends React.Component{
+    render() {
+        
+        return (
+            <>
+            
+            <div className="container">
+                <img class="logo" src="/assets/images/logo192.png"/>
+                <h1 class="form-header">SoulMatch</h1>
+            </div>
+
+            <p>Test text</p>
+            
+            </>
+           
+
+        );
+    
+    }
+
+}
+
+export default PersonalizationPage
