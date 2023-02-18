79a503cf47e375cc17dd19c1b86a0aa3a2adad5e
Mahin Haque <haqumahi27@gmail.com>
2023-02-18 15:47:27 -0500

    Redone dropdown menus to use functions instead for gender selection and preffered gender

--- src/pages/PersonalizationPage.js
+++ src/pages/PersonalizationPage.js
@@ -5,6 +5,7 @@ const genders = ["Male", "Female", "Non-Binary", "Transgender", "Other"]
 
 
 class PersonalizationPage extends React.Component{
+    
     render() {
         
         return (
@@ -18,39 +19,11 @@ class PersonalizationPage extends React.Component{
                 
                 <br></br>
 
-                <div className = "selectGender">
-                    <form>
-                        <label>Please select your gender: </label>
-                        <select>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                            <option value="Non-Binary">Non-Binary</option>
-                            <option value="Transgender">Transgender</option>
-                            <option value="Other">Other</option>
-                        </select>
-                        <input type="submit" value="Submit" />
-                    </form>
-                </div>
+                <GenderDropdown switchFormType={this.switchFormType}/>
 
                 <br></br>
 
-                <div className="selectPreferGender">
-                    <form>
-                        <label>Please select the gender you are 
-                            looking for in your matches:</label>
-                        
-                        <select>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                            <option value="Non-Binary">Non-Binary</option>
-                            <option value="Transgender">Transgender</option>
-                            <option value="Other">Other</option>
-                        </select>
-                        <input type="submit" value="Submit" />
-                    </form>
-                </div>
-                
-                
+                <PreferGenderDropdown switchFormType={this.switchFormType}/>
 
             </body>
         
@@ -63,5 +36,64 @@ class PersonalizationPage extends React.Component{
     }
 
 }
+
+const GenderDropdown = ({switchFormType}) => {
+    const [gender, setGender] = useState('');
+    
+
+    return (
+        <div className='form-box'>
+            
+            <form className='auth-form' onSubmit={() => {
+                
+                console.log(`Setting Gender: ${gender}`)
+            }}>
+                <label>Select your gender: </label>
+                        
+                    <select value={gender} onChange={event => setGender(event.target.value)}>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Non-Binary">Non-Binary</option>
+                        <option value="Transgender">Transgender</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    <input type="submit" value="Submit" />
+
+            </form>
+
+           
+        </div>
+    );
+};
+
+const PreferGenderDropdown = ({switchFormType}) => {
+    const [preferGender, setPreferGender] = useState('');
+    
+
+    return (
+        <div className='form-box'>
+            
+            <form className='auth-form' onSubmit={() => {
+                
+                console.log(`Setting preferred gender: ${preferGender}`)
+            }}>
+                <label>Select the gender you are 
+                            looking for in your matches:</label>
+                        
+                    <select value={preferGender} onChange={event => setPreferGender(event.target.value)}>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Non-Binary">Non-Binary</option>
+                        <option value="Transgender">Transgender</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    <input type="submit" value="Submit" />
+
+            </form>
+
+           
+        </div>
+    );
+};
 
 export default PersonalizationPage
