8f36ae989b0e7e6320dee2f8670b5afb7206f85d
Augustine Jacob <gn2536@wayne.edu>
2023-02-19 18:58:50 -0500

    testing homepage

--- SoulMatch/src/pages/HomePage.js
+++ SoulMatch/src/pages/HomePage.js
@@ -5,11 +5,21 @@ class HomePage extends React.Component {
     render() {
         return (
             <div>
-                <h1>This is the home page</h1>
-                <br/>
-                <h2>We need to make this a proper page, for now it will be a placeholder</h2>
-                <br/>
-                <a href='/login'>Go to login page</a>
+                <body>
+                <picture>
+                    <source media="(min-width: 480px)"
+                        srcset="logo-color.png 1200w, logo-color.png 800w"
+                        sizes="80vw">
+                    </source>
+                    <img src="logo-color.png" alt="Soulmatch logo">
+                    </img>
+                </picture>
+                           
+                        <h1>
+                <a href="../App.js">Log In</a> <br></br>
+                        <a href="">Create Account</a>
+                </h1>
+                </body>
             </div>
         );
     }
