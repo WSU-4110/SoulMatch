a0e01ba2cc077f1005073638df1237c63e179368
Brent Goudie <gw5045@wayne.edu>
2023-02-19 18:41:14 -0500

    updateController code upload

--- SoulMatchServer-main/src/main/java/com/soulmatch/authentication/UpdateController.java
+++ SoulMatchServer-main/src/main/java/com/soulmatch/authentication/UpdateController.java
@@ -1,2 +1,28 @@
-package com.soulmatch.authentication;public class UpdateController {
+package com.soulmatch.authentication;
+
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.soulmatch.firebase.FirebaseController;
+import com.soulmatch.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class UpdateController {
+
+    @Autowired
+    private FirebaseController controller;
+
+    @RequestMapping(
+            value = "/update",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> registerUser(@RequestBody User user) throws InterruptedException, ExecutionException {
+            controller.getFirestore().collection("users").add(user);
+            return ResponseEntity.ok(user);
+    }
 }
