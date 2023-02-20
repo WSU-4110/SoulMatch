cc37420df88f78dd294644ea537eabd7c2355ebc
Augustine Jacob <gn2536@wayne.edu>
2023-02-19 19:05:48 -0500

    testing again. Added style

--- SoulMatch/src/pages/HomePage.js
+++ SoulMatch/src/pages/HomePage.js
@@ -5,6 +5,28 @@ class HomePage extends React.Component {
     render() {
         return (
             <div>
+                <style>
+                    <meta charset="utf-8"> </meta>
+                    <title>SoulMatch</title>
+                    body {
+                        background - color: #15d6a0;
+                    margin: 0 10%;
+                    font-family: sans-serif;
+	            	}
+                    h1 {
+                        text - align: center;
+                    font-family: serif;
+                    font-weight: normal;
+                    text-transform: uppercase;
+                    border-bottom: 1px solid #57b1dc;
+                    margin-top: 30px;
+		            }
+                    h2 {
+                        color: #c7610e;
+                    font-size: 1em;
+		            }
+                </style>
+
                 <body>
                 <picture>
                     <source media="(min-width: 480px)"
