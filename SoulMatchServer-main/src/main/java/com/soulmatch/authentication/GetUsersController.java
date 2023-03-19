0a95b0ccf28d8c543c295788280aa6699c39c088
Brent Goudie <gw5045@wayne.edu>
2023-03-19 16:22:54 -0400

    The profile viewing now sorts by closest scores in a range of +-2

--- SoulMatchServer-main/src/main/java/com/soulmatch/authentication/GetUsersController.java
+++ SoulMatchServer-main/src/main/java/com/soulmatch/authentication/GetUsersController.java
@@ -28,19 +28,49 @@ public class GetUsersController {
     )
     public ResponseEntity<?> getUsersByProfile(@RequestBody User user) {
         user = controller.getUserByEmail(user.getEmail());
+        List<User> userDifferenceOf0 = new ArrayList<>();
+        List<User> userDifferenceOf1 = new ArrayList<>();
+        List<User> userDifferenceOf2 = new ArrayList<>();
         List<User> compatibleUsers = new ArrayList<>();
         for (User user1 : controller.getUsers().values()) {
+            if (user.getId() == user1.getId()){
+                continue;
+            }
 
-            if (UserUtils.doUsersProfileMatch(user, user1)) {
+            if (user1.getProfile().getScore() - user.getProfile().getScore() == -2 || user1.getProfile().getScore() - user.getProfile().getScore() == 2){
                 User u = new User();
                 u.setId(user1.getId());
                 u.setFirstName(user1.getFirstName());
                 u.setLastName(user1.getLastName());
                 u.setBirthday(user1.getBirthday());
                 u.setProfile(user1.getProfile());
-                compatibleUsers.add(u);
+                userDifferenceOf2.add(u);
+            }
+
+            if (user1.getProfile().getScore() - user.getProfile().getScore() == -1 || user1.getProfile().getScore() - user.getProfile().getScore() == 1){
+                User u = new User();
+                u.setId(user1.getId());
+                u.setFirstName(user1.getFirstName());
+                u.setLastName(user1.getLastName());
+                u.setBirthday(user1.getBirthday());
+                u.setProfile(user1.getProfile());
+                userDifferenceOf1.add(u);
+            }
+
+            if (user1.getProfile().getScore() - user.getProfile().getScore() == 0){
+                User u = new User();
+                u.setId(user1.getId());
+                u.setFirstName(user1.getFirstName());
+                u.setLastName(user1.getLastName());
+                u.setBirthday(user1.getBirthday());
+                u.setProfile(user1.getProfile());
+                userDifferenceOf0.add(u);
             }
         }
+
+        compatibleUsers.addAll(userDifferenceOf0);
+        compatibleUsers.addAll(userDifferenceOf1);
+        compatibleUsers.addAll(userDifferenceOf2);
 
         return ResponseEntity.ok(compatibleUsers);
     }
