58ebc5284b7b6f0c060daf7e498455d0fed7b94b
Augustine Jacob <gn2536@wayne.edu>
2023-03-20 20:56:18 -0400

    request to fix auth page

--- SoulMatch/src/pages/AuthenticationPage.js
+++ SoulMatch/src/pages/AuthenticationPage.js
@@ -124,6 +124,34 @@ const RegisterForm = ({switchFormType}) => {
     );
 };
 
+function AgeVerification() {
+    const [age, setAge] = useState('');
+    const [showWarning, setShowWarning] = useState(false);
+  
+    const handleAgeChange = (event) => {
+      setAge(event.target.value);
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (age < 18) {
+        setShowWarning(true);
+      }
+    }
+  
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter your age:
+            <input type="number" value={age} onChange={handleAgeChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        {showWarning && <p>You must be 18 or older to access this site.</p>}
+      </div>
+    );
+  }
 const mapStateToProps = state => {
     return {
         userState: state.user
