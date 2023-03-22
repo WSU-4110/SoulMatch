8da7285c950d39b1f193e732220c496154e238be
Ani180685 <ark180685@gmail.com>
2023-03-21 20:53:41 -0400

    Adding ww4

---
+++ hw4.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import lombok.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+interface WebsocketMessage MessageBrokerConfigurer {
+    public void registerStompEndpoints(StompEndpointRegistry registry)
+    public void configureMessageBroker(MessageBrokerRegistry registry)
+}
+
+public class chatController{
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/message")
+    @SendTo("/chatroom/public")
+    public Message receiveMessage(@Payload Message message){
+        return message;
+    }
+
+    @MessageMapping("/private-message")
+    public Message recievePrivateMessage(@Payload Message message){
+        simpMessagingTemplate.convertAndSendToUser("user", "/private", message);
+        System.out.println(message);
+        return message;
+    }
+} 
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/chatroom","/user");
+        registry.setUserDestinationPrefix("/user");
+    }
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+}
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
+
+main(){
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
+}
