03537782feb1f6bb50823decc1e649a3c9e48b5c
Augustine Jacob <71908659+Aujacob@users.noreply.github.com>
2023-03-27 23:21:15 -0400

    Added navbar to review page

--- SoulMatch/src/pages/ReviewUserPage.js
+++ SoulMatch/src/pages/ReviewUserPage.js
@@ -1,31 +1,38 @@
 import React from "react";
 import {connect} from "react-redux";
 import './../styles/ReviewPage.css';
+import NavbarComponent from "../components/NavbarComponent"; 
 
 class ReviewUserPage extends React.Component {
 
-    //checks if you're logged in or not
+    //checks if the page is loaded
     state = {
         loaded: false
+        //tells the system if it's not loaded that the system is not loaded
     }
 
-    //copy paste
+    //copy paste. If the page is loaded, it tells the system that the page is loaded
     componentDidMount() {
         this.setState({loaded: true});
     }
 
     //copy paste this onto any page
     render() {
-        if (this.state.loaded && this.props.userState.loggedIn) {
+        // so if the page is loaded and user is not logged in, take them to the login page.
+        // and make it so that if they're logged in, they can access the review page.
+        if (this.state.loaded && !this.props.userState.loggedIn) {
             const history = this.props.history;
-            history.push('/home');
+            history.push('/login');
         }
 
         return (
+            //change this name in every page
             <div className="reviewBackground">
                 
+                <NavbarComponent />
+                
 
-                <div className = "homeHeader">
+                {/* <div className = "homeHeader">
                     <a href="/"><img className = "homeLogo" src="/assets/images/soul-match-logo-updated.png" 
                     alt="SoulMatch Logo"/></a>
                     <nav>
@@ -36,16 +43,17 @@ class ReviewUserPage extends React.Component {
                             <li><a href="/contact">Contact</a></li>
                         </ul>
                     </nav>
-                </div>
+                </div> */}
 
-                
+
 
             </div>
         );
     }
 }
 
-//copy paste code into every page
+//copy paste code into every page. 
+// allows the server to access all the user data
 const mapStateToProps = state => {
     return {
         userState: state.user
